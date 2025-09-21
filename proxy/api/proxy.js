df312440314657fb2a05d9fce393f929768e7a7d
optionmain <option.main.inbox@gmail.com>
2025-09-20 20:51:50 -0500

    Update proxy.js

--- proxy/api/proxy.js
+++ proxy/api/proxy.js
@@ -1,25 +1,3 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export default function handler(req, res) {
-  const url = req.query.url;
-  if (!url) {
-    res.status(400).send('Send a URL to proxy, like ?url=https://example.com');
-    return;
-  }
-
-  const proxy = createProxyMiddleware({
-    target: url,
-    changeOrigin: true,
-    pathRewrite: { '^/api/proxy': '' },
-  });
-
-  return proxy(req, res, (err) => {
-    if (err) res.status(500).send('Proxy error');
-  });
+  res.status(200).send('Hello World');
 }
